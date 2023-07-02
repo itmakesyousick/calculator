a08ae4eb61759306d41eecff14136c504e4cff5e
Toki⚡️ <117554130+itmakesyousick@users.noreply.github.com>
2023-07-02 16:10:39 +0900

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,14 @@
+console.clear();
+
+var i = 0;
+var days = 100;
+var capital = 50000;
+var benefit_ratio = 0.05;
+
+
+[...Array(days)].reduce((capital, total) => {
+	total = total || capital;
+	benefit = Math.round(total * benefit_ratio);
+	console.log(total, benefit);
+	return total + benefit;
+}, capital);
