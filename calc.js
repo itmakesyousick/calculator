4826fe7e581cdb158a2f78e13bd4c8c496512b2b
Toki⚡️ <117554130+itmakesyousick@users.noreply.github.com>
2023-07-02 16:18:45 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -6,7 +6,7 @@ let capital = 50000;
 
 [...Array(DAYS)].reduce((capital, total) => {
   total = total || capital;
-	benefit = Math.round(total * INTEREST_RATE);
-	console.log(total, benefit);
-	return total + benefit;
+  benefit = Math.round(total * INTEREST_RATE);
+  console.log(total, benefit);
+  return total + benefit;
 }, capital);
