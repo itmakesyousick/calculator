f27be93f98ea23f07e84f69ceec80661c798aacf
Toki⚡️ <117554130+itmakesyousick@users.noreply.github.com>
2023-07-02 16:16:33 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -5,7 +5,6 @@ var days = 100;
 var capital = 50000;
 var benefit_ratio = 0.05;
 
-
 [...Array(days)].reduce((capital, total) => {
 	total = total || capital;
 	benefit = Math.round(total * benefit_ratio);
