59aab7056b730b3fb9ed829ab076699ca4e1ef4a
Toki⚡️ <117554130+itmakesyousick@users.noreply.github.com>
2023-07-03 22:51:04 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -5,30 +5,31 @@ const tbodyElement = document.getElementsByTagName('tbody')[0];
 const rateElement = document.getElementById('rate-number');
 
 inputCapital.addEventListener('input', () => {
-	if (/[0-9]+/.test(inputCapital.value)) {
+  if (/[0-9]+/.test(inputCapital.value)) {
     calc();
   }
 });
 
 inputRate.addEventListener('input', () => {
-	if (/[0-9.]+/.test(inputRate.value)) {
-		calc();
+  if (/[0-9.]+/.test(inputRate.value)) {
+    calc();
   }
 });
 
 calc();
 
 function calc () {
-	const days = 100;
-	const capital = Number(inputCapital.value);
+  const days = 100;
+  const capital = Number(inputCapital.value);
   const benefit_ratio = Number(inputRate.value);
   const render = renderer(tbodyElement, rateElement, benefit_ratio); 
+  let i = 0;
 
   [...Array(days)].reduce((capital, total) => {
     total = total || capital;
     benefit = Math.round(total * benefit_ratio);
     lot = benefit / (30 * 1000);
-    render(total, benefit, lot);
+    render(i++, total, benefit, lot);
     return total + benefit;
   }, capital);
 }
@@ -37,9 +38,10 @@ function renderer (tbodyElement, rateContainer, benefit_ratio) {
 	tbodyElement.innerHTML = '';
   rateElement.textContent = (benefit_ratio * 100) + '%';
 
-  return (total, benefit, lot) => {
+  return (index, total, benefit, lot) => {
     row = document.createElement('tr');
     row.innerHTML = `
+      <td>${index}</td>
       <td>¥${total.toLocaleString()}</td>
       <td>¥${benefit.toLocaleString()}</td>
       <td title="${Math.round(lot*100000)}">${lot.toFixed(2)}</td>
