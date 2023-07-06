119eece8f6d55a99aa3b614f46a6c7009dee9d00
Toki⚡️ <117554130+itmakesyousick@users.noreply.github.com>
2023-07-07 02:30:31 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -22,7 +22,7 @@ calc();
 function calc () {
   const days = 100;
   const quata_pips = Number(inputCuota.value);
-  const capital = Number(inputCapital.value);
+  const initialCapital = Number(inputCapital.value);
   const benefit_ratio = Number(inputRate.value);
   const render = renderer(tbodyElement, rateElement, benefit_ratio); 
   let i = 0;
@@ -31,23 +31,23 @@ function calc () {
     total = total || capital;
     benefit = Math.round(total * benefit_ratio);
     lot = benefit / (quata_pips * 1000);
-    render(++i, total, benefit, lot, capital);
+    render(++i, total, benefit, lot, initialCapital);
     return total + benefit;
-  }, capital);
+  }, initialCapital);
 }
 
 function renderer (tbodyElement, rateContainer, benefit_ratio) {
   tbodyElement.innerHTML = '';
   rateElement.textContent = (benefit_ratio * 100) + '%';
 
-  return (index, total, benefit, lot, capital) => {
+  return (index, total, benefit, lot, initialCapital) => {
     row = document.createElement('tr');
     row.innerHTML = `
       <td><small>${index}</small></td>
       <td>¥${total.toLocaleString()}</td>
       <td>¥${benefit.toLocaleString()}</td>
       <td title="${Math.round(lot*100000)}">${lot.toFixed(2)} ・ ${Math.round(lot*100000)}</td>
-      <td>¥${(total - capital).toLocaleString()}</td>
+      <td>¥${(total - initialCapital).toLocaleString()}</td>
     `;
     tbodyElement.appendChild(row);
   }
